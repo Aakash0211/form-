96770402f3e59bd5872a59ca670dcfdc00515193
Aakash <akashkushi111@gmail.com>
2021-03-26 17:12:35 +0530

    fourth commit

--- src/Form.js
+++ src/Form.js
@@ -45,8 +45,11 @@ const [log,setLog]=useState(false)
    }
   return (
     <div style={{ textAlign:'center',
-        marginTop:90}}>
-   {log ? (<Typography  variant="h4" style={{paddingBottom:20}}>Log In</Typography>):((<Typography style={{paddingBottom:20}} variant="h4" >Create Account</Typography>))}
+        marginTop:150}}>
+   {log ? (<Typography  variant="h4" style={{paddingBottom:20,fontWeight:'bold'}}>Log In</Typography>):((<div style={{marginRight:170}}><Typography style={{paddingBottom:5,fontWeight:'bold'}} variant="h4" >Create </Typography> 
+   
+    <Typography style={{paddingBottom:15,fontWeight:'bold'}} variant="h4">Account</Typography>
+   </div>))}
       <form onSubmit={formik.handleSubmit} style={{display:'flex',justifyContent:'center',alignItems:'center',flex:1,flexDirection:'column'}}>
       <div style={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
       <TextField
@@ -57,17 +60,24 @@ const [log,setLog]=useState(false)
           style={{borderColor:'#ddd',
           borderWidth:1,
           borderRadius:5,
-          margin:2,
+          margin:5,
           padding:11,
           width:290,
           maxHeight:70
          }}
+         InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              {!formik.errors.email && formik.values.email.length>0 && <CheckCircleOutlineOutlinedIcon fontSize='small'  color='primary' />}
+            </InputAdornment>
+          ),
+        }}
           value={formik.values.email}
           onChange={formik.handleChange}
           error={formik.touched.email && Boolean(formik.errors.email)}
           helperText={formik.touched.email && formik.errors.email}
         />
-     {!formik.errors.email && formik.values.email.length>0 && <CheckCircleOutlineOutlinedIcon fontSize='small'  color='primary' />}
+    
      </div>
      <div style={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
         <TextField
@@ -80,7 +90,7 @@ const [log,setLog]=useState(false)
           borderWidth:1,
           borderRadius:5,
           padding:11,
-          margin:2,
+          margin:5,
           width:290,
           maxHeight:70
          }}
@@ -98,7 +108,7 @@ const [log,setLog]=useState(false)
         />
         
         </div>
-        <div style={{ display:"flex",justifyContent:'space-between',flexDirection:'row',alignItems:'center',paddingHorizontal:20}}>
+        <div style={{ display:"flex",justifyContent:'space-between',flexDirection:'row',alignItems:'center',paddingHorizontal:20,paddingTop:10,paddingBottom:10}}>
         <Checkbox
          checked={checked}
          onChange={()=>{setChecked(!checked)}}
@@ -108,17 +118,17 @@ const [log,setLog]=useState(false)
         </div>
         {log ?(<Button color="primary" variant="contained" style={{padding:6,margin:7}} disabled={!checked}    type="submit">
           Log In
-        </Button>):(<Button color="primary" variant="contained" style={{padding:6,margin:7}} disabled={!checked}  type='submit'>
+        </Button>):(<Button color="primary"  style={{padding:10,margin:10,width:290,maxHeight:60,borderRadius:15}} variant='outlined' disabled={!checked}  type='submit'>
           Create Account
         </Button>)}
-        {!log && <Button color='secondary' style={{padding:6,margin:7}} variant="contained" >
-          Sign Up With Google
+        {!log && <Button  style={{padding:10,margin:10,width:290,maxHeight:60,borderRadius:15,borderColor:'blue'}} variant='outlined' >
+         <div style={{padding:7}}> <img  alt ="Google" src="https://pics.freeicons.io/uploads/icons/png/2659939281579738432-512.png" style={{height:20,width:20}}/> </div> Sign Up With Google
         </Button>}
 
       {!log && (
        <div style={{marginTop:20}}>
       <Typography variant="inherit">Already have an account </Typography>
-      <Button color="primary" variant='text'  onClick={()=>{setLog(!log)}}>Log In</Button>
+      <Button color="primary" variant='text'  onClick={()=>{setLog(!log)}}><Typography style={{textDecorationLine:'underline',textDecorationColor:'blue'}}>Log In</Typography></Button>
        </div>)}
       </form>
     </div>
